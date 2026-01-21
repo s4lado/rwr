72503ea5e37befe7b9ad48c2fc9b6e8723441dd7
Wojciech Czerski <wojteq13@gmail.com>
2026-01-21 22:47:14 +0100

    modified:   docs/index.html 	modified:   docs/index.pck 	modified:   project_files/main.tscn

---
+++ project_files/right_controller.gd
@@ -0,0 +1,53 @@
+extends XRController3D
+
+@onready var ray: RayCast3D = $TeleportRay
+@onready var marker: MeshInstance3D = $TeleportMarker
+var xr_origin: XROrigin3D
+var xr_camera: XRCamera3D
+
+# Flaga sprawdzająca czy właśnie celujemy
+var is_aiming: bool = false
+
+func _ready() -> void:
+	xr_origin = get_parent() as XROrigin3D
+	xr_camera = xr_origin.get_node("XRCamera3D") as XRCamera3D
+	marker.visible = false
+	
+	# Podpinamy sygnały wciśnięcia i puszczenia
+	button_pressed.connect(_on_button_pressed)
+	button_released.connect(_on_button_released)
+
+func _process(_delta: float) -> void:
+	# Pokazuj marker TYLKO jeśli celujemy (trzymamy przycisk) I laser w coś trafia
+	if is_aiming and ray.is_colliding():
+		marker.global_transform.origin = ray.get_collision_point()
+		marker.visible = true
+	else:
+		marker.visible = false
+
+func _on_button_pressed(input_name: String) -> void:
+	if input_name == "trigger_click":
+		is_aiming = true # Zaczynamy celowanie
+
+func _on_button_released(input_name: String) -> void:
+	if input_name == "trigger_click":
+		if is_aiming: # Jeśli celowaliśmy, to teraz skaczemy
+			teleport_now()
+		is_aiming = false # Koniec celowania
+
+func teleport_now() -> void:
+	if not ray.is_colliding():
+		return
+	
+	# ... reszta twojej logiki teleportacji bez zmian ...
+	var target: Vector3 = ray.get_collision_point()
+	var origin_tf := xr_origin.global_transform
+	var cam_tf := xr_camera.global_transform
+	var cam_offset := cam_tf.origin - origin_tf.origin
+	
+	# Ważne: usuwamy komponent Y (wysokość) z offsetu kamery, 
+	# żeby nie wpaść pod podłogę jeśli gracz kuca
+	cam_offset.y = 0 
+
+	origin_tf.origin = target - cam_offset
+	xr_origin.global_transform = origin_tf
